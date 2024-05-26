846005c8a913d291d06f0c61333442f12966dab3
zamal_ <zamalali9866@gmail.com>
2024-05-26 22:16:36 +0200

    Add files via upload

---
+++ config/api_keys.py
@@ -0,0 +1,7 @@
+# config/api_keys.py
+# Store API keys and other sensitive information here.
+# Make sure this file is listed in your .gitignore to prevent exposure.
+
+PINECONE_API_KEY = 'your_pinecone_api_key_here'
+OPENAI_API_KEY = 'your_openai_api_key_here'
+TAVILY_API_KEY = 'your_tavily_api_key_here'
